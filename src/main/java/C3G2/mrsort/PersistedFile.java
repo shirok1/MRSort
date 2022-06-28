4e6728703bb910dceb66f3a4c04da2ff31a7501b
Shiroki Satsuki <tomatroom@qq.com>
2022-06-28 15:40:56 +0800

    feat: PersisteFile MappedByteBuffer

--- src/main/java/C3G2/mrsort/PersistedFile.java
+++ src/main/java/C3G2/mrsort/PersistedFile.java
@@ -7,6 +7,7 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.ByteChannel;
 import java.nio.channels.FileChannel;
 import java.nio.channels.SeekableByteChannel;
@@ -14,6 +15,8 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.concurrent.atomic.AtomicLong;
 
+import static java.nio.channels.FileChannel.MapMode.READ_ONLY;
+import static java.nio.channels.FileChannel.MapMode.READ_WRITE;
 import static java.nio.file.StandardOpenOption.*;
 
 public class PersistedFile {
@@ -34,7 +37,7 @@ public class PersistedFile {
         return path;
     }
 
-    public String getFileName(){
+    public String getFileName() {
         return path.getFileName().toString();
     }
 
@@ -53,7 +56,63 @@ public class PersistedFile {
         return new PersistedFile(target, 0);
     }
 
+
+    public static PersistedFile sortMergeMapped(byte category, byte second, PersistedFile apf, PersistedFile bpf, AtomicLong counter, Path targetDir) {
+        int newLevel = Integer.max(apf.level, bpf.level) + 1;
+        Path target = targetDir.resolve(String.format("%c%c_%d_%d.txt", category, second, newLevel, counter.getAndIncrement()));
+        LOG.info("MERGING {} and {} into {}.", apf.path.getFileName(), bpf.path.getFileName(), target.getFileName());
+        try (FileChannel outChannel = FileChannel.open(target, WRITE, CREATE, TRUNCATE_EXISTING);
+             FileChannel channelA = FileChannel.open(apf.path, READ);
+             FileChannel channelB = FileChannel.open(bpf.path, READ)) {
+            MappedByteBuffer bufferA = channelA.map(READ_ONLY, 0, channelA.size());
+            bufferA.load();
+
+            MappedByteBuffer bufferB = channelB.map(READ_ONLY, 0, channelB.size());
+            bufferB.load();
+
+            MappedByteBuffer outBuffer = outChannel.map(READ_WRITE, 0, channelA.size() + channelB.size());
+
+            while (bufferA.hasRemaining() && bufferB.hasRemaining()) {
+                outBuffer.putLong(bufferA.getLong(bufferA.position()) < bufferB.getLong(bufferB.position())
+                        ? bufferA.getLong() : bufferB.getLong());
+            }
+
+            outBuffer.put(bufferA);
+            outBuffer.put(bufferB);
+
+            outBuffer.force();
+//            Cleaner cleaner = ((DirectBuffer) bufferA).cleaner();
+//            if (cleaner != null) {
+//                cleaner.clean();
+//            }
+//            cleaner = ((DirectBuffer) bufferB).cleaner();
+//            if (cleaner != null) {
+//                cleaner.clean();
+//            }
+            LOG.info("MERGED {} and {} into {}.", apf.path.getFileName(), bpf.path.getFileName(), target.getFileName());
+        } catch (IOException e) {
+            LOG.error("Failed to merge {} and {} into {} :{}", apf.path.getFileName(), bpf.path.getFileName(), target.getFileName(), e);
+        }
+
+        System.gc();
+
+        try {
+            Files.delete(apf.path);
+            Files.delete(bpf.path);
+        } catch (IOException e) {
+            LOG.warn("Failed to delete {} and {}: {}", apf.path.getFileName(), bpf.path.getFileName(), e);
+        }
+        return new PersistedFile(target, newLevel);
+    }
+
     public static PersistedFile sortMerge(byte category, byte second, PersistedFile apf, PersistedFile bpf, AtomicLong counter, Path targetDir) {
+        if(apf.path.toFile().length() <= Integer.MAX_VALUE && bpf.path.toFile().length() <= Integer.MAX_VALUE) {
+            // use MappedByteBuffer
+            return sortMergeMapped(category, second, apf, bpf, counter, targetDir);
+        }
+
+        LOG.warn("Using non-MappedByteBuffer for {} and {}", apf.path.getFileName(), bpf.path.getFileName());
+
         int newLevel = Integer.max(apf.level, bpf.level) + 1;
         Path target = targetDir.resolve(String.format("%c%c_%d_%d.txt", category, second, newLevel, counter.getAndIncrement()));
         LOG.info("MERGING {} and {} into {}.", apf.path.getFileName(), bpf.path.getFileName(), target.getFileName());
